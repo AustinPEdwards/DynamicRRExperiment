d8d866b466df1237bbdaa809f90aa9052919e531
apedwards3 <austinpedwards01@gmail.com>
2023-04-17 00:09:52 -0800

    oop messed up name

--- Experiment.py/Experiment.py/Experiment.py.py
+++ Experiment.py/Experiment.py/Experiment.py.py
@@ -52,8 +52,8 @@ def round_robin(processes, quantum):
             if process != last_run_process:
                 context_switches += 1
             if process.is_completed():
-                process.completion_time = time
-                process.turnaround_time = process.completion_time - process.arrival_time + 1
+                process.completion_time = time + time_executed
+                process.turnaround_time = process.completion_time - process.arrival_time;
                 completed_processes.append(process)
                 last_run_process = None;
                 break
@@ -66,6 +66,91 @@ def round_robin(processes, quantum):
     return completed_processes, context_switches
 
 
+# Round-robin scheduling algorithm
+def efficient_dynamic_round_robin():
+    context_switches = 0
+    time = 0
+    completed_processes = []
+    ready_queue = []
+    n = len(processes)
+    n_ready_queue = len(ready_queue)
+    last_run_process = None;
+    BTmax = 0;
+    index = 1;
+
+    processes = [
+    Process("P1", 0, 80),
+    Process("P2", 0, 45),
+    Process("P3", 0, 62),
+    Process("P4", 0, 34),
+    Process("P4", 0, 78)]
+    for process in processes:
+        ready_queue.append(process)
+
+    # set the time quantum
+    for process in ready_queue:
+        if process.burst_time > BTmax:
+            BTmax = process.burst_time
+            quantum = BTmax * 0.8
+
+
+    while index <= len(ready_queue):
+        if index < len(ready_queue):
+            if ready_queue[index].burst_time <= quantum:
+                # assign cpu to process
+                process = ready_queue.pop(index-1)
+                time_executed = process.execute(quantum)
+                if process != last_run_process:
+                    context_switches += 1
+                if process.is_completed():
+                    process.completion_time = time + time_executed
+                    process.turnaround_time = process.completion_time - process.arrival_time;
+                    completed_processes.append(process)
+                    last_run_process = None;
+                else:
+                    last_run_process = process
+            else:
+                # dont assign and put at back of queue
+                process = ready_queue.pop(index-1)
+                ready_queue.append(process)
+        elif index == len(ready_queue):
+            quantum = BTmax
+            index = 0
+        index += 1
+
+    while len(completed_processes) < n:
+        # Add arriving processes to the ready queue
+        for i in range(n):
+            if processes[i].arrival_time == time:
+                ready_queue.append(processes[i])
+        if last_run_process:
+            ready_queue.append(last_run_process)
+        
+        # Print the current status of the ready queue
+        print(f"Time {time}: [", end="")
+        for process in ready_queue:
+            print(process.name, end=", ")
+        print("]")
+        
+        # Execute each process in the ready queue for the quantum time slice
+        while ready_queue:
+            process = ready_queue.pop(0)
+            time_executed = process.execute(quantum)
+            if process != last_run_process:
+                context_switches += 1
+            if process.is_completed():
+                process.completion_time = time + time_executed
+                process.turnaround_time = process.completion_time - process.arrival_time;
+                completed_processes.append(process)
+                last_run_process = None;
+                break
+            else:
+                last_run_process = process
+                break
+        
+        time += quantum
+    
+    return completed_processes, context_switches
 # Example usage
 processes = [
     Process("A", 0, 3),
@@ -75,6 +160,8 @@ processes = [
     Process("E", 8, 2)]
 quantum = 1
 
+efficient_dynamic_round_robin()
+
 completed_processes, context_switches = round_robin(processes, quantum)
 
 # Print the completion order and turnaround time for each process
@@ -82,3 +169,5 @@ for process in processes:
     print(f"{process.name}: Turnaround time = {process.turnaround_time}")
 
 print(f"context switches: {context_switches}")
+
+efficient_dynamic_round_robin()
