ab448e9bfbc27b18a967a4db14e2faf45e930998
apedwards3 <austinpedwards01@gmail.com>
2023-04-21 22:06:14 -0800

    commented

--- Experiment.py/Experiment.py/Experiment.py.py
+++ Experiment.py/Experiment.py/Experiment.py.py
@@ -1,8 +1,23 @@
+#########################################################################
+#
+#     Dynamic Round-Robin CPU Scheduling Algorithm Experiment
+#     Austin Edwards & William Spear 
+#     April 21,  2023
+#     CSCE 321 - Operating Systems
+#
+#     Features Efficient Dynamic Round Robin
+#              Smart Dynamic Round Robin
+#              Modified Median Round-Robin Algorithm
+#              Standard Rounbd Robin (used as a baseline)
+#
+#########################################################################
+
 import math
 from math import sqrt
 import random
 import copy
 from statistics import median, mean
+import random
 
 
 # Process class to represent each process
@@ -29,9 +44,13 @@ class Process:
     def is_completed(self):
         return self.remaining_time == 0
 
+#########################################################
+#
+#     Implementation of Round Robin Algorithm
+#
+#########################################################
 def round_robin(processes, quantum):
     quantum = 2
-    ##print("Time Quantum: ", quantum)
     context_switches = 0
     time = 0
     completed_processes = []
@@ -40,54 +59,53 @@ def round_robin(processes, quantum):
     last_run_process = None;
     
     while len(completed_processes) < n:
-        # Add arriving processes to the ready queue
+
+        # adds arriving processes to the ready queue
         processes_to_remove = []
         for process in processes:
             if process.arrival_time <= time:
                 ready_queue.append(process)
                 processes_to_remove.append(process)
                 added_process = True
-
         for process in processes_to_remove:
             processes.remove(process)
         if last_run_process:
             ready_queue.append(last_run_process)
 
+        # if no processes are in the ready queue, increment the time and continue
         if not ready_queue:
             time += 1
             continue
 
-        #print(f"Time {time}: [", end="")
-        #for process in ready_queue:
-        #    print(process.name, end=", ")
-        #print("]")
-
-        while ready_queue:
-            process = ready_queue.pop(0)
-            #print(process.name, end=": ")
-            time_executed = process.execute(quantum)
-            if not process.first_accessed:
-                process.first_time = time
-                process.first_accessed = True
-            #print(time_executed)
-            if process != last_run_process:
-                context_switches += 1
-            if process.is_completed():
-                process.completion_time = time + time_executed
-                process.turnaround_time = process.completion_time - process.arrival_time;
-                completed_processes.append(process)
-                last_run_process = None;
-                break
-            else:
-                last_run_process = process
-                break
+        # completes a time slice from the next avaliable process
+        process = ready_queue.pop(0)
+        time_executed = process.execute(quantum)
+        if not process.first_accessed:
+            process.first_time = time
+            process.first_accessed = True
+        if process != last_run_process:
+            context_switches += 1
+        if process.is_completed():
+            process.completion_time = time + time_executed
+            process.turnaround_time = process.completion_time - process.arrival_time;
+            completed_processes.append(process)
+            last_run_process = None;
+        else:
+            last_run_process = process
         
+        # increment the time appropriatly
         time += time_executed
     
     return completed_processes, context_switches
 
 
-# Round-robin scheduling algorithm
+
+
+#################################################################
+#
+#     Implementation of Efficient Dynamic Round Robin Algorithm
+#
+#################################################################
 def efficient_dynamic_round_robin(processes):
     context_switches = 0
     time = 0
@@ -97,6 +115,7 @@ def efficient_dynamic_round_robin(processes):
     last_run_process = None;
     added_process = False;
 
+    # adds arriving processes to the ready queue
     while len(completed_processes) < n:
         added_process = False
         processes_to_remove = []
@@ -109,37 +128,33 @@ def efficient_dynamic_round_robin(processes):
         for process in processes_to_remove:
             processes.remove(process)
 
+        # if no processes are in the ready queue, increment the time and continue
         if not ready_queue:
             time += 1
             continue
-        # set the time quantum
+
+        # dynamically set the time quantum
         BTmax = 0
         if added_process:
             for process in ready_queue:
                 if process.remaining_time > BTmax:
                     BTmax = process.remaining_time
                     quantum = BTmax * 0.8
-                    #print("Time Quantum: ", quantum)
 
+        # if all processes burt times are greater than the time quantum, set the time quantum to the max
         if all(process.remaining_time > quantum for process in ready_queue):
             quantum = max(process.remaining_time for process in ready_queue)
         
-        #print(f"Time {time}: [", end="")
-        #for process in ready_queue:
-        #    print(process.name, end=", ")
-        #print("]")
-        processes_to_move_to_back = []
         # Execute each process in the ready queue for the quantum time slice
+        processes_to_move_to_back = []
         for process_index, process in enumerate(ready_queue):
             if len(ready_queue) == 1:
                 if process != last_run_process:
                     context_switches += 1
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 if process.is_completed():
                     process.completion_time = time + time_executed
                     process.turnaround_time = process.completion_time - process.arrival_time;
@@ -154,12 +169,10 @@ def efficient_dynamic_round_robin(processes):
             elif process.remaining_time <= quantum:
                 if process != last_run_process:
                     context_switches += 1
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 process.completion_time = time + time_executed
                 process.turnaround_time = process.completion_time - process.arrival_time
                 completed_processes.append(process)
@@ -169,17 +182,23 @@ def efficient_dynamic_round_robin(processes):
             else:
                 processes_to_move_to_back.append(process)
 
+        # append uncompleted processes to the ready queue
         for process in processes_to_move_to_back:
             ready_queue.remove(process)
         for process in processes_to_move_to_back:
             ready_queue.append(process)
 
-
+        # increment the time appropriatly
         time += time_executed
     
     return completed_processes, context_switches
 
 
+#################################################################
+#
+#     Implementation of Smart Dynamic Round Robin Algorithm
+#
+#################################################################
 def smart_dynamic_round_robin(processes):
     context_switches = 0
     time = 0
@@ -194,7 +213,7 @@ def smart_dynamic_round_robin(processes):
         added_process = False
         processes_to_remove = []
 
-        # move processes that have arrived into the ready_queue
+        # adds arriving processes to the ready queue
         for process in processes:
             if process.arrival_time <= time:
                 ready_queue.append(process)
@@ -205,29 +224,22 @@ def smart_dynamic_round_robin(processes):
             first_process = ready_queue[0]
             count = 0
 
-        # if nothing in ready queue, continue to next time step
+        # if no processes are in the ready queue, increment the time and continue
         if not ready_queue:
             time += 1
             continue
 
-        # sort the processes
+        # sort the processes and dynamically calculate the time quantum
         ready_queue = sorted(ready_queue, key=lambda process: process.remaining_time)
         first_process = ready_queue[0]
-        # calculate the differences between adjacent burst times
         differences = [ready_queue[i+1].remaining_time - ready_queue[i].remaining_time for i in range(len(ready_queue)-1)]
-        # calculate the average of the differences
         if len(differences) > 0:
             STQ = round(sum(differences) / len(differences))
             Delta = STQ // 2
             if STQ == 0:
                 STQ += 1
 
-
-
-        #print(f"Time {time}: [", end="")
-        #for process in ready_queue:
-        #    print(process.name, end=", ")
-        #print("]")
+        # Execute each process in the ready queue for the quantum time slice
         process_index = 0
         actual_index = 0
         number_of_processes = len(ready_queue)
@@ -235,12 +247,10 @@ def smart_dynamic_round_robin(processes):
             process = ready_queue[actual_index]
             if (len(ready_queue) == 1) or (process.remaining_time <= (STQ + Delta)):
                 quantum = process.remaining_time
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 if process != last_run_process:
                     context_switches += 1
                 process.completion_time = time + time_executed
@@ -253,20 +263,24 @@ def smart_dynamic_round_robin(processes):
                 quantum = STQ
                 if process != last_run_process:
                     context_switches += 1
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 process_index += 1
                 actual_index += 1
 
+            # increment the time appropriatly
             time += time_executed
     
     return completed_processes, context_switches
 
 
+#################################################################
+#
+#     Implementation of Modified Median Round Robin Algorithm
+#
+#################################################################
 def modified_median_dynamic_round_robin(processes):
     context_switches = 0
     time = 0
@@ -281,7 +295,7 @@ def modified_median_dynamic_round_robin(processes):
         added_process = False
         processes_to_remove = []
 
-        # move processes that have arrived into the ready_queue
+        # adds arriving processes to the ready queue
         for process in processes:
             if process.arrival_time <= time:
                 ready_queue.append(process)
@@ -292,41 +306,32 @@ def modified_median_dynamic_round_robin(processes):
             first_process = ready_queue[0]
             count = 0
 
-        # if nothing in ready queue, continue to next time step
+        # if no processes are in the ready queue, increment the time and continue
         if not ready_queue:
             time += 1
             continue
 
-        # sort the processes
+        # sort the ready queue and dynamically calculate the time quantum
         ready_queue = sorted(ready_queue, key=lambda process: process.remaining_time)
-        # Create a list of remaining times
         remaining_times = [process.remaining_time for process in ready_queue]
-        # Calculate the median remaining time
         median_BT = median(remaining_times)
         max_BT = ready_queue[-1:][0].remaining_time
         TQ = round(sqrt(median_BT*max_BT))
-        #print("Time Quantum: ", TQ)
-
         threshold = .2
         
-        #print(f"Time {time}: [", end="")
-        #for process in ready_queue:
-        #    print(process.name, end=", ")
-        #print("]")
 
         process_index = 0
         actual_index = 0
+        # completes a time slice from the next avaliable process
         number_of_processes = len(ready_queue)
         while process_index < number_of_processes:
             process = ready_queue[actual_index]
             if (len(ready_queue) == 1) or (process.remaining_time <= (process.burst_time*threshold)) or (process.remaining_time <= TQ):
                 quantum = process.remaining_time
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 if process != last_run_process:
                     context_switches += 1
                 process.completion_time = time + time_executed
@@ -339,21 +344,24 @@ def modified_median_dynamic_round_robin(processes):
                 quantum = TQ
                 if process != last_run_process:
                     context_switches += 1
-                #print(process.name, end=": ")
                 time_executed = process.execute(quantum)
                 if not process.first_accessed:
                     process.first_time = time
                     process.first_accessed = True
-                #print(time_executed)
                 process_index += 1
                 actual_index += 1
 
+            # increment the time appropriatly
             time += time_executed
     
     return completed_processes, context_switches
 
-import random
 
+#################################################################
+#
+#     Generates random processes
+#
+#################################################################
 def generate_processes(num_processes, min_burst_time, max_burst_time, min_arrival_time, max_arrival_time):
     processes = []
     for i in range(num_processes):
@@ -369,15 +377,9 @@ def generate_processes(num_processes, min_burst_time, max_burst_time, min_arriva
     
     return processes
 
-
+# generate 50 processes with burst times between 0-40 and arrival times between 0-1,000
 processes = generate_processes(50, 0, 40, 0, 1000)
 
-#processes = [
-#    Process("P1", 0, 5),
-#    Process("P2", 0, 7),
-#    Process("P3", 0, 2),
-#    Process("P4", 0, 4),
-#    Process("P5", 0, 9)]
 
 quantum = 20
 
@@ -390,13 +392,10 @@ total_turnaround_time = 0
 total_waiting_time = 0
 total_response_time = 0
 for process in completed_processes:
-    #print(f"{process.name}: Arrival time = {process.arrival_time}, Burst time = {process.burst_time}, Turnaround time = {process.turnaround_time}, Waiting time = {process.completion_time - process.arrival_time - process.burst_time}")
     total_turnaround_time += process.turnaround_time
     total_waiting_time += process.completion_time - process.arrival_time - process.burst_time
-    total_response_time += process.first_time
+    total_response_time += process.first_time - process.arrival_time
 
-maximum_turnaround_time = max(process.turnaround_time for process in completed_processes)
-maximum_wait_time = max((process.completion_time - process.arrival_time - process.burst_time) for process in completed_processes)
 average_turnaround_time = total_turnaround_time / len(completed_processes)
 average_waiting_time = total_waiting_time / len(completed_processes)
 average_response_time = total_response_time / len(completed_processes)
@@ -411,13 +410,11 @@ total_turnaround_time = 0
 total_waiting_time = 0
 total_response_time = 0
 for process in completed_processes:
-    #print(f"{process.name}: Arrival time = {process.arrival_time}, Burst time = {process.burst_time}, Turnaround time = {process.turnaround_time}, Waiting time = {process.completion_time - process.arrival_time - process.burst_time}")
     total_turnaround_time += process.turnaround_time
     total_waiting_time += process.completion_time - process.arrival_time - process.burst_time
-    total_response_time += process.first_time
+    total_response_time += process.first_time - process.arrival_time
 
-maximum_turnaround_time = max(process.turnaround_time for process in completed_processes)
-maximum_wait_time = max((process.completion_time - process.arrival_time - process.burst_time) for process in completed_processes)
+
 average_turnaround_time = total_turnaround_time / len(completed_processes)
 average_waiting_time = total_waiting_time / len(completed_processes)
 average_response_time = total_response_time / len(completed_processes)
@@ -431,10 +428,9 @@ total_turnaround_time = 0
 total_waiting_time = 0
 total_response_time = 0
 for process in completed_processes:
-    #print(f"{process.name}: Arrival time = {process.arrival_time}, Burst time = {process.burst_time}, Turnaround time = {process.turnaround_time}, Waiting time = {process.completion_time - process.arrival_time - process.burst_time}")
     total_turnaround_time += process.turnaround_time
     total_waiting_time += process.completion_time - process.arrival_time - process.burst_time
-    total_response_time += process.first_time
+    total_response_time += process.first_time - process.arrival_time
 
 maximum_turnaround_time = max(process.turnaround_time for process in completed_processes)
 maximum_wait_time = max((process.completion_time - process.arrival_time - process.burst_time) for process in completed_processes)
@@ -452,10 +448,9 @@ total_turnaround_time = 0
 total_waiting_time = 0
 total_response_time = 0
 for process in completed_processes:
-    #print(f"{process.name}: Arrival time = {process.arrival_time}, Burst time = {process.burst_time}, Turnaround time = {process.turnaround_time}, Waiting time = {process.completion_time - process.arrival_time - process.burst_time}")
     total_turnaround_time += process.turnaround_time
     total_waiting_time += process.completion_time - process.arrival_time - process.burst_time
-    total_response_time += process.first_time
+    total_response_time += process.first_time - process.arrival_time
 
 maximum_turnaround_time = max(process.turnaround_time for process in completed_processes)
 maximum_wait_time = max((process.completion_time - process.arrival_time - process.burst_time) for process in completed_processes)
